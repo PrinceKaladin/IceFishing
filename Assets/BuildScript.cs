586a5b676afb3f83289a392836ffd4f0b57b0f97
PrinceKaladin <abdulaziz01022007@gmail.com>
2025-12-18 18:29:21 +0300

    Add files via upload

---
+++ Assets/BuildScript.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+
+public class BuildScript
+{
+    public static void PerformBuild()
+    {
+        string[] scenes = {
+            "Assets/Scenes/settings.unity",
+            "Assets/Scenes/gameplay.unity",
+            "Assets/Scenes/lose.unity",
+            "Assets/Scenes/win.unity",
+            "Assets/Scenes/menu.unity",
+            "Assets/Scenes/howtoplay.unity"
+        };
+
+        string buildPath = "build/Android/IceFishing.apk";
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = buildPath,
+            target = BuildTarget.Android,
+            options = BuildOptions.None
+        };
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}

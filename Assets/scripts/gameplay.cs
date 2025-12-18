36a158eebba12238fddca3152dfef8617d22df72
PrinceKaladin <abdulaziz01022007@gmail.com>
2025-12-18 18:22:47 +0300

    К

---
+++ Assets/scripts/gameplay.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameplay : MonoBehaviour
+{
+    public GameObject boot;
+    public GameObject fish;
+    public Sprite ul;
+    public GameObject udochka;
+    public void Ulov() {
+        int i = Random.Range(0, 3);
+        udochka.GetComponent<SpriteRenderer>().sprite = ul;
+        if (i == 0) {
+            PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score") + 10);
+
+            StartCoroutine(x());  
+            
+        }
+        else
+        {
+            
+            StartCoroutine (y());
+        }
+
+    }
+    IEnumerator x() { 
+        fish.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(4);
+    }
+    IEnumerator y()
+    {
+        boot.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(5);
+    }
+}
---
+++ Assets/scripts/score.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("score"))
+        {
+            PlayerPrefs.SetInt("score", 0);
+        }
+        this.GetComponent<Text>().text =  PlayerPrefs.GetInt("score").ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
